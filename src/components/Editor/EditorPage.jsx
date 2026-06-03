f1eea939357a664465c5c18380fdc0237a68c63b
YashKrTripathi <yashkr.tripathi@gmail.com>
2026-06-03 13:01:08 +0530

    fix(webrtc): Resolve ghost connections and video stream race conditions

--- src/components/Editor/EditorPage.jsx
+++ src/components/Editor/EditorPage.jsx
@@ -1775,6 +1775,7 @@ export default function EditorPage({ user }) {
       {showVideoCall && room.roomId && (
         <VideoCall
           roomId={room.roomId}
+          userId={user?.uid}
           userName={user?.displayName || user?.email?.split('@')[0] || 'Guest'}
           onClose={() => setShowVideoCall(false)}
         />
